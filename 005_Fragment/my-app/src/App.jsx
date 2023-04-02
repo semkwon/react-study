e82e91ae9c4bd5bdda0d163cc6fd375eb67e4cf6
권세명 <3pkwon@gmail.com>
2023-04-02 22:14:22 +0900

    005 Fragment

---
+++ 005_Fragment/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>안녕, 라이캣 1호!</h1>
+      <h1>안녕, 라이캣 2호!</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
